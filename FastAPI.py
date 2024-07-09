4216552a926660dc1a2ff461d0e5f04009b8f661
ThRahmani <73460752+ThRahmani@users.noreply.github.com>
2024-07-09 06:57:35 +0330

    Add files via upload

--- FastAPI.py
+++ FastAPI.py
@@ -1,30 +1,71 @@
 from fastapi import FastAPI
-import json
+from StatisticalAnalysis import *
+from fastapi.responses import HTMLResponse
 app = FastAPI()
-@app.get("/data/")
+
+
+@app.get("/api/data/", response_class=HTMLResponse)
 def data():
-    pass
-@app.get('/summary/')
+    # return {'info: ': housing.describe(),
+    #         'Data: ': dict(housing.iloc[:5,:5])}
+    # تبدیل دیتافریم به HTML
+    html_table = housing.iloc[:5,:].to_html(index=False)
+    html_describe = housing.describe().to_html()
+    # قالب HTML برای نمایش جدول
+    html_content = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>DataFrame Table</title>
+        <style>
+            table {{
+                width: 50%;
+                margin: 25px auto;
+                border-collapse: collapse;
+                text-align: left;
+            }}
+            th, td {{
+                padding: 12px;
+                border-bottom: 1px solid #ddd;
+            }}
+            th {{
+                background-color: #f2f2f2;
+            }}
+        </style>
+    </head>
+    <body>
+        <h1>DataFrame Table</h1>
+        {html_table}
+        <h2>Describe Table</h2>
+        {html_describe}
+    </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content)
+
+@app.get('/api/summary/')
 def summary():
     pass
-@app.get('/visualization/')
-def geraph():
+
+@app.get('/api/visualization/')
+def graph():
     pass
-@app.get('/corrolation/')
+
+@app.get('/api/correlation/')
 def cor():
     pass
-@app.post('/preprocess/')
-def preprocess(content:json):
+
+@app.post('/api/preprocess/')
+def preprocess(content: dict):
     pass
-@app.post('/patterns/')
-def patterns(content:json):
+
+@app.post('/api/patterns/')
+def patterns(content: dict):
     pass
-@app.get('/insights/')
+
+@app.get('/api/insights/')
 def insights():
     pass
-@app.get('/limitations/')
-def limit():
-    pass
-@app.get('/future research/')
-def advice():
-    pass
+
+
+
